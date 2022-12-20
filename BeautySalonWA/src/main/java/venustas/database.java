b69b5c54e12f8810d872668cad3afc00c79696c7
Najma8 <98664050+Najma8@users.noreply.github.com>
2022-12-20 23:37:01 +0300

    Update database.java

--- BeautySalonWA/src/main/java/venustas/database.java
+++ BeautySalonWA/src/main/java/venustas/database.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
@@ -24,7 +25,7 @@ public class database {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13");
             Statement stmt = con.createStatement();
-            stmt.execute("INSERT INTO kullanici (name,email,sifre) Values('" + name + "','" + email +"','"+ sifre + "')");
+            stmt.execute("INSERT INTO kullanici (name,email,sifre) Values('" + name + "','" + email + "','" + sifre + "')");
             System.out.println("oldu");
             con.close();
         } catch (Exception e) {
@@ -32,35 +33,56 @@ public class database {
         }
     }
 
-    public boolean giriskontrol(String email, String sifre) {
-        boolean girisyapilsinmi=false;
+    public giris giriskontrol(String email, String sifre) {
+        giris g = new giris();
+        g.setGirisdurumu(false);
+        boolean girisyapilsinmi = false;
         try {
+            int id = 0;
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("Select * from kullanici where email='" + email + "' and sifre='" + sifre + "'");
             while (rs.next()) {
-               girisyapilsinmi=true;
+                g.setGiristipi("kullanici");
+                g.setGirisdurumu(true);
+                girisyapilsinmi = true;
+                id = rs.getInt("id");
+            }
+            rs = stmt.executeQuery("Select * from calisan where email='" + email + "' and sifre='" + sifre + "'");
+            while (rs.next()) {
+                g.setGiristipi("calisan");
+                g.setGirisdurumu(true);
+                girisyapilsinmi = true;
+                id = rs.getInt("id");
+            }
+            if (g.getGiristipi() != null) {
+                if (g.getGiristipi() == "kullanici") {
+                    stmt.execute("INSERT INTO giris (giristipi,kullaniciid) Values('" + g.getGiristipi() + "'," + id + ")");
+                } else if (g.getGiristipi() == "calisan") {
+                    stmt.execute("INSERT INTO giris (giristipi,calisanid) Values('" + g.getGiristipi() + "'," + id + ")");
+                }
             }
             con.close();
 
         } catch (Exception e) {
             out.println(e);
         }
-        return girisyapilsinmi;
-        
+        return g;
+
     }
-        public boolean kayitlimikontrol(String email) {
-            boolean kayitlimi=false;
+
+    public boolean kayitlimikontrol(String email) {
+        boolean kayitlimi = false;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("Select * from kullanici where email='" + email + "'");
             while (rs.next()) {
-                
-              kayitlimi=true;
-                
+
+                kayitlimi = true;
+
             }
             con.close();
 
@@ -69,6 +91,40 @@ public class database {
         }
         return kayitlimi;
     }
-    
-    
+
+    public boolean RandevuKontrolu(String telno, String yas, String time, String kategori, String email, String not) {
+        boolean olusturdumu = false;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13");
+            Statement stmt = con.createStatement();
+            LocalDateTime dateTime = LocalDateTime.parse(time);
+            ResultSet rs = stmt.executeQuery("Select * from randevu where tarih='" + dateTime + "'");
+            if (!rs.next()) {
+                stmt.execute("UPDATE `kullanici` SET `telno` = '" + telno + "', `yas` = '" + yas + "' WHERE `email` ='" + email + "'");
+                int kategoriid = 0;
+                int kullaniciid = 0;
+                ResultSet rs2 = stmt.executeQuery("Select * from kategori where name='" + kategori + "'");
+                while (rs2.next()) {
+                    kategoriid = rs2.getInt("id");
+                }
+                ResultSet rs3 = stmt.executeQuery("Select * from kullanici where email='" + email + "'");
+                while (rs3.next()) {
+                    kullaniciid = rs3.getInt("id");
+                }
+                if (!not.equals("")) {
+                    stmt.execute("INSERT INTO `randevu` (`tarih`, `kategoriid`, `kullaniciid`, `not`) VALUES ('" + dateTime + "'," + kategoriid + "," + kullaniciid + ",'" + not + "')");
+                } else {
+                    stmt.execute("INSERT INTO randevu (tarih,kategoriid,kullaniciid) Values('" + dateTime + "'," + kategoriid + "," + kullaniciid + ")");
+                }
+
+                olusturdumu = true;
+            }
+            con.close();
+
+        } catch (Exception e) {
+            out.println(e);
+        }
+        return olusturdumu;
+    }
 }
