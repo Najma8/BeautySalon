27b3a297a09ee99a0eff704faa30a3ae3bdf144c
Najma8 <98664050+Najma8@users.noreply.github.com>
2022-12-29 21:02:49 +0300

    Update database.java

--- BeautySalonWA/src/main/java/venustas/database.java
+++ BeautySalonWA/src/main/java/venustas/database.java
@@ -25,7 +25,8 @@ import java.util.Map;
  * @author ESMA
  */
 public class database {
-   public void insert(String name, String email, String sifre) {
+
+    public void insert(String name, String email, String sifre) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13");
@@ -43,7 +44,7 @@ public class database {
         try {
             int id = 0;
             Class.forName("com.mysql.jdbc.Driver");
-           try ( Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13")) {
+            try ( Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13")) {
                 PreparedStatement ps = con.prepareStatement("Select * from kullanici where email=? and sifre=?");
 
                 Statement stmt = con.createStatement();
@@ -51,30 +52,30 @@ public class database {
                 ps.setString(2, sifre);
 
                 ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                g.setGiristipi("kullanici");
-                g.setGirisdurumu(true);
-                id = rs.getInt("id");
-            }
+                while (rs.next()) {
+                    g.setGiristipi("kullanici");
+                    g.setGirisdurumu(true);
+                    id = rs.getInt("id");
+                }
                 ps = con.prepareStatement("Select * from calisan where email=? and sifre=?");
                 ps.setString(1, email);
                 ps.setString(2, sifre);
-              
-              rs = ps.executeQuery();
-            //rs = stmt.executeQuery("Select * from calisan where email='" + email + "' and sifre='" + sifre + "'");
-            while (rs.next()) {
-                g.setGiristipi("calisan");
-                g.setGirisdurumu(true);
-                id = rs.getInt("id");
-            }
-            if (g.getGiristipi() != null) {
-               if ("kullanici".equals(g.getGiristipi())) {
+
+                rs = ps.executeQuery();
+                //rs = stmt.executeQuery("Select * from calisan where email='" + email + "' and sifre='" + sifre + "'");
+                while (rs.next()) {
+                    g.setGiristipi("calisan");
+                    g.setGirisdurumu(true);
+                    id = rs.getInt("id");
+                }
+                if (g.getGiristipi() != null) {
+                    if ("kullanici".equals(g.getGiristipi())) {
                         stmt.execute("INSERT INTO giris (giristipi,kullaniciid) Values('" + g.getGiristipi() + "'," + id + ")");
                     } else if ("calisan".equals(g.getGiristipi())) {
                         stmt.execute("INSERT INTO giris (giristipi,calisanid) Values('" + g.getGiristipi() + "'," + id + ")");
                     }
+                }
             }
-          }
         } catch (ClassNotFoundException | SQLException e) {
             out.println(e);
         }
@@ -116,53 +117,53 @@ public class database {
                 Statement stmt = con.createStatement();
                 LocalDateTime dateTime = LocalDateTime.parse(time);
                 ResultSet rs = stmt.executeQuery("Select * from randevu where tarih='" + dateTime + "'");
-           
-       
-            if (!rs.next()) {
-                stmt.execute("UPDATE `kullanici` SET `telno` = '" + telno + "', `yas` = '" + yas + "' WHERE `email` ='" + email + "'");
-                int kategoriid = 0;
-                int kullaniciid = 0;
-                 PreparedStatement ps = con.prepareStatement("Select * from kategori where name=?");
-                    ps.setString(1, name);
+
+                if (!rs.next()) {
+                    stmt.execute("UPDATE `kullanici` SET `telno` = '" + telno + "', `yas` = '" + yas + "' WHERE `email` ='" + email + "'");
+                    int kategoriid = 0;
+                    int kullaniciid = 0;
+                    PreparedStatement ps = con.prepareStatement("Select * from kategori where name=?");
+                    ps.setString(1, kategori);
 
                     ResultSet rs2 = ps.executeQuery();
-                while (rs2.next()) {
-                    tekrarlamasuresi = rs2.getInt("tekrarlamasuresi");
-                    seanssayisi = rs2.getInt("seanssayisi");
-                    kategoriid = rs2.getInt("id");
-                }
-                ps = con.prepareStatement("Select * from kullanici where email=?");
-
-                ps.setString(1, email);
-                ResultSet rs3 = ps.executeQuery();
-               
-               // ResultSet rs3 = stmt.executeQuery("Select * from kullanici where email='" + email + "'");
-                while (rs3.next()) {
-                    kullaniciid = rs3.getInt("id");
-                }
-                int degisimsayisi;
-                for (int i = 1; i <= seanssayisi; i++) {
-                    if (!not.equals("")) {
-                        stmt.execute("INSERT INTO `randevu` (`tarih`, `kategoriid`, `kullaniciid`, `not`) VALUES ('" + dateTime + "'," + kategoriid + "," + kullaniciid + ",'" + not + "')");
-                    } else {
-                        stmt.execute("INSERT INTO randevu (tarih,kategoriid,kullaniciid) Values('" + dateTime + "'," + kategoriid + "," + kullaniciid + ")");
+                    while (rs2.next()) {
+                        tekrarlamasuresi = rs2.getInt("tekrarlamasuresi");
+                        seanssayisi = rs2.getInt("seanssayisi");
+                        kategoriid = rs2.getInt("id");
                     }
-                    dateTime = dateTime.plusDays(tekrarlamasuresi);
-                    do {
-                        degisimsayisi = 0;
-                        ResultSet rset = stmt.executeQuery("Select * from randevu where tarih='" + dateTime + "'");
-                        while (rset.next()) {
-                            dateTime = dateTime.plusHours(1);
-                            degisimsayisi = 1;
+                    ps = con.prepareStatement("Select * from kullanici where email=?");
+
+                    ps.setString(1, email);
+                    ResultSet rs3 = ps.executeQuery();
+
+                    // ResultSet rs3 = stmt.executeQuery("Select * from kullanici where email='" + email + "'");
+                    while (rs3.next()) {
+                        kullaniciid = rs3.getInt("id");
+                    }
+                    int degisimsayisi;
+                    for (int i = 1; i <= seanssayisi; i++) {
+                        if (!not.equals("")) {
+                            stmt.execute("INSERT INTO `randevu` (`tarih`, `kategoriid`, `kullaniciid`, `not`) VALUES ('" + dateTime + "'," + kategoriid + "," + kullaniciid + ",'" + not + "')");
+                        } else {
+                            stmt.execute("INSERT INTO randevu (tarih,kategoriid,kullaniciid) Values('" + dateTime + "'," + kategoriid + "," + kullaniciid + ")");
                         }
-                    } while (degisimsayisi == 1);
+                        dateTime = dateTime.plusDays(tekrarlamasuresi);
+                        do {
+                            degisimsayisi = 0;
+                            ResultSet rset = stmt.executeQuery("Select * from randevu where tarih='" + dateTime + "'");
+                            while (rset.next()) {
+                                dateTime = dateTime.plusHours(1);
+                                degisimsayisi = 1;
+                            }
+                        } while (degisimsayisi == 1);
 
+                    }
+
+                    olusturdumu = true;
                 }
+                con.close();
 
-                olusturdumu = true;
             }
-            con.close();
-
         } catch (ClassNotFoundException | SQLException e) {
             out.println(e);
         }
@@ -183,88 +184,90 @@ public class database {
         int kullaniciid = 0;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-          
-           try ( Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13")) {
+
+            try ( Connection con = DriverManager.getConnection("jdbc:mysql://app.sobiad.com:3306/grup13?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "grup13", "grup13")) {
                 Statement stmt = con.createStatement();
-                
-                PreparedStatement ps=con.prepareStatement("Select * from kullanici where email=?");
-            ps.setString(1, email);
-            ResultSet rs=ps.executeQuery();
 
-              //  ResultSet rs = stmt.executeQuery("Select * from kullanici where email='" + email + "'");
-            while (rs.next()) {
-                kullaniciid = rs.getInt("id");
-            }
-            rs = stmt.executeQuery("SELECT * FROM randevu INNER JOIN kategori ON randevu.kategoriid = kategori.id WHERE randevu.kullaniciid =" + kullaniciid);
-            while (rs.next()) {
-                String sqldatetime = rs.getTimestamp("tarih").toString();
-                String kategori = rs.getString("name");
-                datetime.add(sqldatetime);
-                kategoriadi.add(kategori);
-            }
-            con.close();
-            for (int i = 0; i < datetime.size(); i++) {
-                String[] testdizi = datetime.get(i).split(" ");
-                String teststring = testdizi[0];
-                date.add(teststring);
-                teststring = testdizi[1];
-                time.add(teststring);
-            }
-            int x = 0;
-            int y = 0;
-            for (int i = 0; i < date.size(); i++) {
-                String[] testdizi = date.get(i).split("-");
+                PreparedStatement ps = con.prepareStatement("Select * from kullanici where email=?");
+                ps.setString(1, email);
+                ResultSet rs = ps.executeQuery();
 
-                try {
+                //  ResultSet rs = stmt.executeQuery("Select * from kullanici where email='" + email + "'");
+                while (rs.next()) {
+                    kullaniciid = rs.getInt("id");
+                }
+                rs = stmt.executeQuery("SELECT * FROM randevu INNER JOIN kategori ON randevu.kategoriid = kategori.id WHERE randevu.kullaniciid =" + kullaniciid);
+                while (rs.next()) {
+                    String sqldatetime = rs.getTimestamp("tarih").toString();
+                    String kategori = rs.getString("name");
+                    datetime.add(sqldatetime);
+                    kategoriadi.add(kategori);
+                }
+                con.close();
+                for (int i = 0; i < datetime.size(); i++) {
+                    String[] testdizi = datetime.get(i).split(" ");
                     String teststring = testdizi[0];
-                    dateint.add(Integer.parseInt(teststring));
+                    date.add(teststring);
                     teststring = testdizi[1];
-                    dateint.add(Integer.parseInt(teststring));
-                    teststring = testdizi[2];
-                    dateint.add(Integer.parseInt(teststring));
-                    x = x + 3;
-                } catch (Exception e) {
-                    System.out.println(e);
+                    time.add(teststring);
                 }
-            }
-            for (int i = 0; i < time.size(); i++) {
-                String[] testdizi = time.get(i).split(":");
-                try {
-                    String teststring = testdizi[0];
-                    String teststring2 = testdizi[1];
-                    starttime.add(teststring + ":" + teststring2);
-                } catch (Exception e) {
-                    System.out.println(e);
+                int x = 0;
+                int y = 0;
+                for (int i = 0; i < date.size(); i++) {
+                    String[] testdizi = date.get(i).split("-");
+
+                    try {
+                        String teststring = testdizi[0];
+                        dateint.add(Integer.parseInt(teststring));
+                        teststring = testdizi[1];
+                        dateint.add(Integer.parseInt(teststring));
+                        teststring = testdizi[2];
+                        dateint.add(Integer.parseInt(teststring));
+                        x = x + 3;
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
                 }
+                for (int i = 0; i < time.size(); i++) {
+                    String[] testdizi = time.get(i).split(":");
+                    try {
+                        String teststring = testdizi[0];
+                        String teststring2 = testdizi[1];
+                        starttime.add(teststring + ":" + teststring2);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+                int j = 0;
+                int k = 0;
+                for (int i = 0; i < kategoriadi.size(); i++) {
+
+                    Map events = new LinkedHashMap();
+                    events.put("title", kategoriadi.get(i));
+                    events.put("time", starttime.get(i));
+
+                    JSONArray eventsarray = new JSONArray();
+                    eventsarray.add(events);
+
+                    Map randevubilgisi = new LinkedHashMap();
+                    randevubilgisi.put("day", dateint.get(j + 2));
+                    randevubilgisi.put("month", dateint.get(j + 1));
+                    randevubilgisi.put("year", dateint.get(j));
+                    randevubilgisi.put("events", eventsarray);
+
+                    array.add(randevubilgisi);
+
+                    j = j + 3;
+                    k = k + 2;
+                }
+                System.out.println(array.toString());
             }
-            int j = 0;
-            int k = 0;
-            for (int i = 0; i < kategoriadi.size(); i++) {
-
-                Map events = new LinkedHashMap();
-                events.put("title", kategoriadi.get(i));
-                events.put("time", starttime.get(i));
-
-                JSONArray eventsarray = new JSONArray();
-                eventsarray.add(events);
-
-                Map randevubilgisi = new LinkedHashMap();
-                randevubilgisi.put("day", dateint.get(j + 2));
-                randevubilgisi.put("month", dateint.get(j + 1));
-                randevubilgisi.put("year", dateint.get(j));
-                randevubilgisi.put("events", eventsarray);
-
-                array.add(randevubilgisi);
-
-                j = j + 3;
-                k = k + 2;
-            }
-            System.out.println(array.toString());
         } catch (Exception e) {
             out.println(e);
         }
         return array;
     }
+
     public JSONArray CalisanRandevu(String email) {
         JSONObject j1 = new JSONObject();
         ArrayList<String> datetime = new ArrayList<>();
@@ -356,5 +359,3 @@ public class database {
         return array;
     }
 }
- 
-
